95c57f206013dc5f93b4deefca0f1a6c7178c4aa
Abhi <Abhi347@users.noreply.github.com>
2022-09-25 18:15:52 +0200

    fix: Use cached octokit

--- src/github.ts
+++ src/github.ts
@@ -1,14 +1,23 @@
 import {getOctokit} from '@actions/github';
+import {GitHub} from '@actions/github/lib/utils';
 
 const GH_TOKEN = process.env.GITHUB_TOKEN || process.env.TEST_TOKEN || '';
 
-const octokit = getOctokit(GH_TOKEN);
+let octokit: InstanceType<typeof GitHub>;
+
+const getCachedOctokit = () => {
+  if (octokit) {
+    return octokit;
+  }
+  octokit = getOctokit(GH_TOKEN);
+  return octokit;
+};
 
 const FULL_REPO_NAME = process.env.GITHUB_REPOSITORY;
 const [OWNER, REPO] = FULL_REPO_NAME?.split('/') || '/';
 
 export const readRepoFile = async (filePath: string) => {
-  return octokit.rest.repos.getContent({
+  return getCachedOctokit().rest.repos.getContent({
     owner: OWNER,
     repo: REPO,
     path: filePath,
